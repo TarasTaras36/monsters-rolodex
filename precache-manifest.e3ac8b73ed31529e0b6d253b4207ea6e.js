645fe8297f47abb0c4459877fc16acf73468dd33
TarasTaras36 <dr.tarasaleksandrov@gmail.com>
2020-05-06 18:17:58 +0300

    Updates

---
+++ precache-manifest.e3ac8b73ed31529e0b6d253b4207ea6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155b742ebc4c4879685ffb82dd435406",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "94247cd670c46ec0f8a1",
+    "url": "/monster-rolodex/static/css/main.3132f8fb.chunk.css"
+  },
+  {
+    "revision": "678ad42ba4717cf6fa77",
+    "url": "/monster-rolodex/static/js/2.89330057.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.89330057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94247cd670c46ec0f8a1",
+    "url": "/monster-rolodex/static/js/main.4ed5cc91.chunk.js"
+  },
+  {
+    "revision": "0e8d2f615b89a212eb3c",
+    "url": "/monster-rolodex/static/js/runtime-main.8e62af18.js"
+  }
+]);
